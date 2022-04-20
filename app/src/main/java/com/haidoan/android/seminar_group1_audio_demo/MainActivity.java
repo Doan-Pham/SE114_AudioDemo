c9af48ef52af82e5798d76c0057429cd01abf6d0
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-04-20 13:38:26 +0700

    Load audio from resource and play with MediaPlayer

--- app/src/main/java/com/haidoan/android/seminar_group1_audio_demo/MainActivity.java
+++ app/src/main/java/com/haidoan/android/seminar_group1_audio_demo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.haidoan.android.seminar_group1_audio_demo;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.julius_marx_julius_marx_vices);
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer != null){
+                    mediaPlayer.start();
+                }
+            }
+        });
     }
 }
